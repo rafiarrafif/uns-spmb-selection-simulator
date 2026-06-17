a5926fb8b7c9984ba94cb7e4a3e9a8490cd8dd36
rafiarrafif <rafiarrafif05@gmail.com>
2026-06-17 21:05:03 +0700

    👔 feat: add administrative screening logic

--- src/main/java/org/sda/modules/stepTwo/Screening.java
+++ src/main/java/org/sda/modules/stepTwo/Screening.java
@@ -1,6 +1,8 @@
 package org.sda.modules.stepTwo;
 
 import tech.tablesaw.api.Table;
+import tech.tablesaw.columns.Column;
+import tech.tablesaw.selection.Selection;
 
 /**
  * Screening pada kasus penerimaan UNS adalah sebagai berikut:
@@ -10,11 +12,34 @@ import tech.tablesaw.api.Table;
 public class Screening {
 
     public static Table main(Table studentData) {
-        System.out.println("Total Data Peserta yang akan discreening: " + studentData.rowCount());
-        Table screenedData = studentData
-            .where(studentData.intColumn("Tahun Lulus").isGreaterThanOrEqualTo(2023))
-            .dropRowsWithMissingValues();
-        System.out.println("Total Data Peserta setelah screening: " + screenedData.rowCount());
-        return screenedData;
+        try {
+            System.out.println(
+                "Total Data Peserta yang akan discreening: " + studentData.rowCount()
+            );
+
+            Selection hasNullSelection = Selection.withRange(0, 0);
+            for (Column<?> col : studentData.columns()) {
+                hasNullSelection = hasNullSelection.or(col.isMissing());
+            }
+
+            Selection lulusTerlambatSelection = studentData
+                .intColumn("Tahun Lulus")
+                .isLessThan(2023);
+            Selection gagalSelection = hasNullSelection.or(lulusTerlambatSelection);
+            Selection allRows = Selection.withRange(0, studentData.rowCount());
+            Selection lolosSelection = allRows.andNot(gagalSelection);
+
+            Table lolosData = studentData.where(lolosSelection);
+            Table gagalData = studentData.where(gagalSelection);
+
+            System.out.println("Total Data Peserta setelah screening : " + lolosData.rowCount());
+            System.out.println("Total Data Peserta gagal seleksi    : " + gagalData.rowCount());
+
+            gagalData.write().csv("src/main/output/peserta-gagal-administrasi.csv");
+            return lolosData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error occurred while screening student data", e);
+        }
     }
 }
